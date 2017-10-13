8ffcb608fddfe4897cad591dbc5f3778283d33b4
ivan delcuadri <ivandelcuadri@hotmail.com>
2017-10-13 11:56:50 -0300

    conexion a bd

--- GymSoft/src/conectorBD/MySQLconexion.java
+++ GymSoft/src/conectorBD/MySQLconexion.java
@@ -1,5 +1,28 @@
 package conectorBD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLconexion {
 
+	public static Connection getConexion(){
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url="jdbc:mysql://localhost/gymsoft";
+			String usuario="root";
+			String contraseña="";
+			con = DriverManager.getConnection(url, usuario, contraseña);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error --> al cargar Driver");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Error --> al conectar BD");
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
 }
