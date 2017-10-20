77f4411f2b7b20897c05e4984212153ba84c567f
ivan delcuadri <ivandelcuadri@hotmail.com>
2017-10-20 20:30:49 -0300

    dispose on close actividad

--- GymSoft/src/interfaz/NuevaActividad.java
+++ GymSoft/src/interfaz/NuevaActividad.java
@@ -45,7 +45,7 @@ public class NuevaActividad extends JFrame {
 	public NuevaActividad() {
 		setResizable(false);
 		setTitle("Nueva Actividad");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
